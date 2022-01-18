5a043eb1030f9ea456776cdd826c2b064a9b5721
Danny Kerr <danny.w.kerr@gmail.com>
2022-01-18 21:57:27 +1030

    Improved test animation

--- gui.py
+++ gui.py
@@ -15,14 +15,21 @@ class MainGUI(tkinter.Tk):
     def __init__(self):
         super().__init__()
         self.title('Hypotrochoid Generator')
+
         self.height = 800
         self.width = 800
         self.geometry(f'{self.height}x{self.width}')
 
+        self.center_x = self.height // 2
+        self.center_y = self.width // 2
+
         self.outer_circle = None
         self.inner_circle = None
         self.pendulum = None
+
         self.inner_circle_radius = 100
+        self.inner_circle_x = self.center_x
+        self.inner_circle_y = self.center_y
         self.outer_circle_radius = 200
 
         self.main_canvas = tkinter.Canvas(
@@ -40,7 +47,7 @@ class MainGUI(tkinter.Tk):
 
         self.draw_initial_setup()
 
-    def circle(self, x=0, y=0, r=0):
+    def circle(self, x=0.0, y=0.0, r=0.0):
         """Draw a circle on the main canvas with specified attributes"""
         return self.main_canvas.create_oval(x + r, y + r, x - r, y - r)
 
@@ -71,14 +78,31 @@ class MainGUI(tkinter.Tk):
         for i in range(1, 361):
             time.sleep(1 / 60)
             self.main_canvas.delete(self.pendulum)
+            self.main_canvas.delete(self.inner_circle)
+            self.main_canvas.delete(all)
+
+            self.inner_circle_x, self.inner_circle_y = geometry.polar_to_cartesian_with_offset(
+                r=self.inner_circle_radius,
+                theta=i,
+                x_offset=self.center_x,
+                y_offset=self.center_y
+            )
+
+            self.inner_circle = self.circle(
+                x=self.inner_circle_x,
+                y=self.inner_circle_y,
+                r=self.inner_circle_radius
+            )
+            
             self.pendulum = self.main_canvas.create_line(
-                self.height // 2,
-                self.width // 2,
+                self.inner_circle_x,
+                self.inner_circle_y,
                 geometry.polar_to_cartesian_with_offset(
                     r=self.inner_circle_radius,
-                    theta=i,
-                    x_offset=self.height // 2,
-                    y_offset=self.width // 2
+                    theta=i*2,
+                    x_offset=self.inner_circle_x,
+                    y_offset=self.inner_circle_y
                 )
             )
             self.update()
+            self.update_idletasks()
