e44f5be4f2943bb9c1313b71459d2e854198cdf5
Danny Kerr <danny.w.kerr@gmail.com>
2022-01-21 10:10:26 +1030

    Implement ABC for drawing classes

---
+++ geometry.py
@@ -0,0 +1,124 @@
+from abc import ABC, abstractmethod
+import math
+
+
+class CanvasDrawing(ABC):
+
+    @abstractmethod
+    def draw(self):
+        pass
+
+    @abstractmethod
+    def delete(self):
+        pass
+
+
+class Circle(CanvasDrawing):
+    """Defines linked list of circles on canvas"""
+
+    def __init__(self, r: float = 10, theta: float = 0, canvas=None, parent=None):
+        self.center_x = 0.0
+        self.center_y = 0.0
+        self.radius = r
+        self.canvas = canvas
+        self.parent = parent
+        self.theta = theta
+        self.theta_mod = 1
+        self.calculate_position()
+        self.canvas_repr = None
+
+    def calculate_position(self) -> None:
+        """Calculate current position based on current properties"""
+        if self.parent is None:
+            self.center_x = self.canvas.center_x
+            self.center_y = self.canvas.center_y
+
+        else:
+            self.center_x, self.center_y = polar_to_cartesian_with_offset(
+                r=self.parent.radius - self.radius,
+                theta=self.theta * self.theta_mod,
+                x_offset=self.parent.center_x,
+                y_offset=self.parent.center_y
+            )
+
+    def draw(self) -> None:
+        """Draw a circle on the main canvas with specified dimensions and location"""
+        x = self.center_x
+        y = self.center_y
+        r = self.radius
+        self.canvas_repr = self.canvas.create_oval(x + r, y + r, x - r, y - r)
+
+    def delete(self) -> None:
+        self.canvas.delete(self.canvas_repr)
+
+
+class Arm(ABC):
+
+    def __init__(self, parent: Circle, canvas):
+        self.canvas = canvas
+        self.parent = parent
+        self.start_coords = (self.parent.center_x, self.parent.center_y)
+        self.end_coords = (0, 0)
+        self.length_mod = 1
+        self.theta = 1
+        self.theta_mod = 1
+        self.canvas_repr = None
+
+    def calculate_position(self) -> None:
+        """Calculate arm start and end position"""
+        self.start_coords = (self.parent.center_x, self.parent.center_y)
+        self.end_coords = polar_to_cartesian_with_offset(
+            r=self.parent.radius * self.length_mod,
+            theta=self.theta * self.theta_mod,
+            x_offset=self.parent.center_x,
+            y_offset=self.parent.center_y
+        )
+
+    def draw(self) -> None:
+        self.canvas_repr = self.canvas.create_line(
+            self.start_coords,
+            self.end_coords
+        )
+
+    def delete(self) -> None:
+        self.canvas.delete(self.canvas_repr)
+
+
+class Tracer(CanvasDrawing):
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.canvas_repr = None
+        self.coords = []
+        self.colour = 'red'
+
+    def draw(self):
+        self.canvas_repr = self.canvas.create_line(
+                    self.coords,
+                    width=1,
+                    smooth=True,
+                    fill=self.colour,
+                )
+
+    def delete(self):
+        self.canvas.delete(self.canvas_repr)
+
+
+# def cartesian_to_polar(x=0.0, y=0.0) -> tuple[float, float]:
+#     """Takes cartesian coordinates as input and returns polar coordinates"""
+#     r = math.sqrt(x ** 2 + y ** 2)
+#     theta = math.atan(y / x)
+#     return r, theta
+#
+#
+# def polar_to_cartesian(r=0.0, theta=0.0) -> tuple[float, float]:
+#     """Takes polar coordinates as input and returns cartesian coordinates"""
+#     x = r * math.cos(math.radians(theta))
+#     y = r * math.sin(math.radians(theta))
+#     return x, y
+
+
+def polar_to_cartesian_with_offset(r=0.0, theta=0.0, x_offset=0.0, y_offset=0.0) -> tuple[float, float]:
+    """Takes polar coordinates as input and returns cartesian coordinates"""
+    x = (r * math.cos(math.radians(theta))) + x_offset
+    y = (r * math.sin(math.radians(theta))) + y_offset
+    return x, y
--- gui.py
+++ gui.py
@@ -5,11 +5,12 @@ GUI setup and drawing functions
 
 import tkinter
 import time
-import math
+import geometry
 
 
 class MainCanvas(tkinter.Canvas):
     """Defines custom canvas and animation class"""
+
     def __init__(self, parent):
         super().__init__()
 
@@ -26,19 +27,13 @@ class MainCanvas(tkinter.Canvas):
         self.center_y = self.width // 2
 
         self.parent = parent
-        self.fps = 240
+        self.fps = 60
 
-        self.trace = None
-        self.trace_coords = []
+        self.tracer = None
 
         self.circles = []
         self.inner_circle = None
-
-        self.pendulum = None
-        self.pendulum_end_x = 0
-        self.pendulum_end_y = 0
-        self.pendulum_theta_mod = 5 / 3
-        self.pendulum_length_mod = 5 / 3
+        self.arm = None
 
         self.playback_stopped = True
         self.playback_frame = 1
@@ -51,34 +46,26 @@ class MainCanvas(tkinter.Canvas):
     def draw_initial_setup(self) -> None:
         """Draw all components on canvas at default settings"""
         self.circles = [
-            Circle(300, 0, self, None),
+            geometry.Circle(300, 0, self, None),
         ]
 
         self.circles.append(
-            Circle(180, 0, self, self.circles[0])
+            geometry.Circle(180, 0, self, self.circles[0])
         )
 
         self.circles.append(
-            Circle(30, 0, self, self.circles[1])
+            geometry.Circle(120, 0, self, self.circles[1])
         )
 
         for circle in self.circles:
             circle.draw()
 
         self.inner_circle = self.circles[-1]
-        self.pendulum = self.create_line(
-            self.calculate_pendulum_coords(self.inner_circle.radius, 0)
-        )
 
-    def calculate_pendulum_coords(self, r, theta) -> tuple[float, float, float, float]:
-        """Calculate pendulum start and end position"""
-        self.pendulum_end_x, self.pendulum_end_y = polar_to_cartesian_with_offset(
-            r=r * self.pendulum_length_mod,
-            theta=theta * self.pendulum_theta_mod,
-            x_offset=self.inner_circle.center_x,
-            y_offset=self.inner_circle.center_y
-        )
-        return self.inner_circle.center_x, self.inner_circle.center_y, self.pendulum_end_x, self.pendulum_end_y
+        self.arm = geometry.Arm(self.circles[-1], self)
+        self.arm.draw()
+
+        self.tracer = geometry.Tracer(self)
 
     def reset_playback(self) -> None:
         """Reset paused playback and redraw initial setup"""
@@ -94,12 +81,27 @@ class MainCanvas(tkinter.Canvas):
         """Sets flag so that drawing stops on next trace calculation"""
         self.playback_stopped = True
 
+    def apply_mods(self):
+        self.arm.length_mod = 1.1
+        self.circles[1].radius = 194.7
+        self.circles[1].theta_mod = -1
+        self.circles[2].radius = 30
+
+    def calculate_positions(self, i) -> None:
+        """Calculates all canvas drawing object positions"""
+        for circle in self.circles:
+            if circle.parent is not None:
+                circle.theta = circle.parent.radius / circle.radius * i
+            circle.calculate_position()
+        self.arm.theta = -self.arm.parent.theta * (self.arm.parent.parent.radius / self.arm.parent.radius)
+        self.arm.calculate_position()
+        self.tracer.coords.append(self.arm.end_coords)
+
     def animate_test(self) -> None:
         """Test animation - pendulum rotating 360° inside the inner circle"""
-        # TEMPORARY - Mess with theta mods for chaos testing
-        self.circles[1].theta_mod = -1
-        self.trace_coords = []
+        self.tracer.coords = []
         self.playback_stopped = False
+        self.apply_mods()
 
         # i represents number of rotations to calculate
         for i in range(self.playback_frame, 36001):
@@ -120,101 +122,40 @@ class MainCanvas(tkinter.Canvas):
             self.delete("all")
 
             # Draw circles
+            self.calculate_positions(i)
             for circle in self.circles:
-                circle.theta = i
-                circle.calculate_position()
-
-                if self.hide_drawing:
-                    circle.draw()
-
-            # Draw pendulum
-
-            if self.hide_drawing:
-                self.pendulum = self.create_line(
-                    self.calculate_pendulum_coords(self.inner_circle.radius, -i)
-                )
-            else:
-                self.calculate_pendulum_coords(self.inner_circle.radius, -i)
+                circle.draw()
+            self.arm.draw()
 
             # Add to coords list every 4 calculations
             # Needs to be adjustable, affects performance and quality
-            self.trace_coords.append((self.pendulum_end_x, self.pendulum_end_y))
+            self.tracer.coords.append(self.arm.end_coords)
 
             # Lower Z-index of all circles and pendulum so trace is more visible
-            self.tag_lower(self.pendulum)
+            self.tag_lower(self.arm)
             for circle in self.circles:
                 self.tag_lower(circle)
 
-            # Only begin drawing after 8 frames to allow minimum coordinates in list
-            if i > 7:
-                self.trace = self.create_line(
-                    self.trace_coords,
-                    width=1,
-                    smooth=1,
-                    fill='red',
-                )
+            # Only begin drawing after 2 frames to allow minimum coordinates in list
+            if i > 1:
+                self.tracer.draw()
 
             self.update_idletasks()
             self.update()
 
     def draw_many(self) -> None:
         """Calculates and draws a specified number of frames in one step"""
-        self.trace_coords = []
-        self.pendulum_length_mod = 2.0
-        self.circles[1].theta_mod = -2
-        self.pendulum_theta_mod = 0.391
+        self.playback_stopped = True
+        self.tracer.coords = []
         self.delete('all')
+        self.apply_mods()
 
         for i in range(0, 40000):
-            for circle in self.circles:
-                circle.theta = i
-                circle.calculate_position()
-            self.calculate_pendulum_coords(self.inner_circle.radius, i)
-            self.trace_coords.append((self.pendulum_end_x, self.pendulum_end_y))
+            self.calculate_positions(i / 10)
 
-        self.trace = self.create_line(
-            self.trace_coords,
-            width=1,
-            smooth=1,
-            fill='red',
-        )
+        self.tracer.draw()
         self.update_idletasks()
         self.update()
-
-
-class Circle:
-    """Defines linked list of circles on canvas"""
-
-    def __init__(self, r: float = 10, theta: float = 0, canvas: MainCanvas = None, parent=None):
-        self.center_x = 0.0
-        self.center_y = 0.0
-        self.radius = r
-        self.canvas = canvas
-        self.parent = parent
-        self.theta = theta
-        self.theta_mod = 1
-        self.calculate_position()
-
-    def calculate_position(self) -> None:
-        """Calculate current position based on current properties"""
-        if self.parent is None:
-            self.center_x = self.canvas.center_x
-            self.center_y = self.canvas.center_y
-
-        else:
-            self.center_x, self.center_y = polar_to_cartesian_with_offset(
-                r=self.parent.radius - self.radius,
-                theta=self.theta * self.theta_mod,
-                x_offset=self.parent.center_x,
-                y_offset=self.parent.center_y
-            )
-
-    def draw(self) -> classmethod:
-        """Draw a circle on the main canvas with specified dimensions and location"""
-        x = self.center_x
-        y = self.center_y
-        r = self.radius
-        return self.canvas.create_oval(x + r, y + r, x - r, y - r)
 
 
 class MainGUI(tkinter.Tk):
@@ -260,15 +201,11 @@ class MainGUI(tkinter.Tk):
         )
         self.hide_circles.pack()
 
-    def test_check(self):
-        print(self.main_canvas.hide_drawing.get())
-
     def play_button(self) -> None:
         """Define behaviour of play button"""
         if self.main_canvas.playback_stopped:
             self.main_canvas.playback_stopped = False
             self.main_canvas.resume_playback()
-
         else:
             self.main_canvas.animate_test()
 
@@ -281,7 +218,6 @@ class MainGUI(tkinter.Tk):
             self.stop_button.configure(
                 text='STOP'
             )
-
         else:
             self.main_canvas.stop_playback()
             self.stop_button.configure(
@@ -290,24 +226,3 @@ class MainGUI(tkinter.Tk):
             self.play_button.configure(
                 text='RESUME'
             )
-
-
-def cartesian_to_polar(x=0.0, y=0.0) -> tuple[float, float]:
-    """Takes cartesian coordinates as input and returns polar coordinates"""
-    r = math.sqrt(x ** 2 + y ** 2)
-    theta = math.atan(y / x)
-    return r, theta
-
-
-def polar_to_cartesian(r=0.0, theta=0.0) -> tuple[float, float]:
-    """Takes polar coordinates as input and returns cartesian coordinates"""
-    x = r * math.cos(math.radians(theta))
-    y = r * math.sin(math.radians(theta))
-    return x, y
-
-
-def polar_to_cartesian_with_offset(r=0.0, theta=0.0, x_offset=0.0, y_offset=0.0) -> tuple[float, float]:
-    """Takes polar coordinates as input and returns cartesian coordinates"""
-    x = (r * math.cos(math.radians(theta))) + x_offset
-    y = (r * math.sin(math.radians(theta))) + y_offset
-    return x, y
