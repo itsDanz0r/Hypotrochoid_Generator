d89edd9d5b40f24da09a27ce6a122071a8f90dce
Danny Kerr <danny.w.kerr@gmail.com>
2022-01-21 15:38:00 +1030

    Added high res png output

--- gui.py
+++ gui.py
@@ -6,6 +6,17 @@ GUI setup and drawing functions
 import tkinter
 import time
 import geometry
+from PIL import Image
+
+
+class SidebarFrame(tkinter.Frame):
+    def __init__(self):
+        super().__init__()
+
+
+class CanvasFrame(tkinter.Frame):
+    def __init__(self):
+        super().__init__()
 
 
 class MainCanvas(tkinter.Canvas):
@@ -40,28 +51,34 @@ class MainCanvas(tkinter.Canvas):
         self.hide_drawing = tkinter.BooleanVar()
         self.hide_drawing.set(False)
 
-        self.draw_initial_setup()
+        self.initial_setup()
 
-    def draw_initial_setup(self) -> None:
+    def initial_setup(self) -> None:
         """Draw all components on canvas at default settings"""
         self.circles = [
-            geometry.Circle(300, 0, self, None),
+            geometry.Circle(400, 0, self, None),
         ]
 
         self.circles.append(
-            geometry.Circle(180, 0, self, self.circles[0])
+            geometry.Circle(200.1, 0, self, self.circles[0])
         )
 
         self.circles.append(
-            geometry.Circle(120, 0, self, self.circles[1])
+            geometry.Circle(100, 0, self, self.circles[1])
         )
+
+        self.inner_circle = self.circles[-1]
+        for i in range(len(self.circles)):
+            if i % 2 != 0:
+                self.circles[i].theta_mod = -1
+
+        self.arm = geometry.Arm(self.circles[-1], self)
+        self.apply_mods()
+        self.arm.calculate_position()
 
         for circle in self.circles:
             circle.draw()
 
-        self.inner_circle = self.circles[-1]
-
-        self.arm = geometry.Arm(self.circles[-1], self)
         self.arm.draw()
 
         self.tracer = geometry.Tracer(self)
@@ -69,22 +86,20 @@ class MainCanvas(tkinter.Canvas):
     def reset_playback(self) -> None:
         """Reset paused playback and redraw initial setup"""
         self.delete('all')
-        self.draw_initial_setup()
+        self.initial_setup()
         self.playback_frame = 1
 
     def resume_playback(self) -> None:
         """Resume paused playback"""
-        self.animate_test()
+        self.animate()
 
     def stop_playback(self) -> None:
         """Sets flag so that drawing stops on next trace calculation"""
         self.playback_stopped = True
 
     def apply_mods(self):
-        self.arm.length_mod = 1.1
-        self.circles[1].radius = 194.7
-        self.circles[1].theta_mod = -1
-        self.circles[2].radius = 30
+        self.arm.theta_mod = -1
+        self.arm.length_mod = 1
 
     def calculate_positions(self, i) -> None:
         """Calculates all canvas drawing object positions"""
@@ -96,23 +111,15 @@ class MainCanvas(tkinter.Canvas):
         self.arm.calculate_position()
         self.tracer.coords.append(self.arm.end_coords)
 
-    def animate_test(self) -> None:
+    def animate(self) -> None:
         """Test animation - pendulum rotating 360° inside the inner circle"""
         self.tracer.coords = []
         self.playback_stopped = False
         self.apply_mods()
 
         # i represents number of rotations to calculate
-        for i in range(self.playback_frame, 36001):
-            if self.playback_stopped:
-                self.playback_frame = i
-                self.parent.stop_button.configure(
-                    text='RESET'
-                )
-                self.parent.play_button.configure(
-                    text='RESUME'
-                )
-                return
+        while not self.playback_stopped:
+            self.playback_frame += 1
 
             # Division here determines frame rate if program running full speed
             time.sleep(1 / self.fps)
@@ -121,14 +128,10 @@ class MainCanvas(tkinter.Canvas):
             self.delete("all")
 
             # Draw circles
-            self.calculate_positions(i)
+            self.calculate_positions(self.playback_frame)
             for circle in self.circles:
                 circle.draw()
             self.arm.draw()
-
-            # Add to coords list every 4 calculations
-            # Needs to be adjustable, affects performance and quality
-            self.tracer.coords.append(self.arm.end_coords)
 
             # Lower Z-index of all circles and pendulum so trace is more visible
             self.tag_lower(self.arm)
@@ -136,10 +139,9 @@ class MainCanvas(tkinter.Canvas):
                 self.tag_lower(circle)
 
             # Only begin drawing after 2 frames to allow minimum coordinates in list
-            if i > 1:
+            if self.playback_frame > 2:
                 self.tracer.draw()
 
-            self.update_idletasks()
             self.update()
 
     def draw_many(self) -> None:
@@ -149,12 +151,26 @@ class MainCanvas(tkinter.Canvas):
         self.delete('all')
         self.apply_mods()
 
-        for i in range(0, 40000):
-            self.calculate_positions(i / 10)
+        print('Calculating...')
+        for i in range(0, 3600000):
+            self.calculate_positions(i/1000)
 
         self.tracer.draw()
-        self.update_idletasks()
-        self.update()
+
+        self.create_img()
+
+    def create_img(self):
+        img = Image.new('RGB', (24000, 24000), 'white')
+        pixels = img.load()
+        rounded_coords = []
+        print('Generating image...')
+        for i in self.tracer.coords:
+            rounded_coords.append((round(i[0] * 20), round(i[1] * 20)))
+        for i in range(len(rounded_coords)):
+            img.putpixel(rounded_coords[i][0], rounded_coords[i][1], (0, 0, 0))
+
+        img.save(r"C:\users\DK\desktop\test.png")
+        img.show()
 
 
 class MainGUI(tkinter.Tk):
@@ -204,19 +220,20 @@ class MainGUI(tkinter.Tk):
             self.main_canvas.playback_stopped = False
             self.main_canvas.resume_playback()
         else:
-            self.main_canvas.animate_test()
+            self.main_canvas.animate()
 
     def stop_button(self) -> None:
         """Define behaviour of stop button"""
         if self.main_canvas.playback_stopped:
             self.main_canvas.playback_stopped = False
             self.main_canvas.delete('all')
-            self.main_canvas.draw_initial_setup()
+            self.main_canvas.initial_setup()
             self.stop_button.configure(
                 text='STOP'
             )
         else:
             self.main_canvas.stop_playback()
+            self.main_canvas.playback_frame = 0
             self.stop_button.configure(
                 text='RESET'
             )
